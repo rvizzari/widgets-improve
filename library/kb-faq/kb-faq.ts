e46f04ff478bcd3e52bbb3a95463a6b07dfeb3ee
Vizzari, Ramiro (Ramiro) **CTR** <rvizzari@avaya.com>
2020-06-18 14:34:59 -0300

    Add Copyright header

--- library/kb-faq/kb-faq.ts
+++ library/kb-faq/kb-faq.ts
@@ -1,3 +1,19 @@
+/*
+ *
+ * Copyright Avaya Inc., All Rights Reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Avaya Inc.
+ *
+ * The copyright notice above does not evidence any actual or intended
+ * publication of such source code.
+ *
+ * Some third-party source code components may have  been modified from their
+ * original versions by Avaya Inc.
+ *
+ * The modifications are Copyright Avaya Inc., All Rights Reserved.
+ *
+ */
+
 interface IKbFaqScope extends ng.IScope {
   widgetConfig: any;
   suggestions: IKbFaqSuggestion[];
