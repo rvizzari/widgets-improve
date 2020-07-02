e2a65043292b91161a06420d0d1c5ec8c0cefa20
Vizzari, Ramiro (Ramiro) **CTR** <rvizzari@avaya.com>
2020-07-02 10:09:12 -0300

    Add service parameter Add service parameter in order to create the lib/*.service.ts structure on widget creation

--- Gruntfile.js
+++ Gruntfile.js
@@ -241,11 +241,14 @@ module.exports = function(grunt) {
     var tpl_html = '<div class="neo-widget widget--<%= element %>">\n\n  <div class="neo-widget__header aoc-home"><%= name %></div>\n\n  <div class="neo-widget__content neo-widget__content--indented">\n    <div class="row">\n      <div class="col-medium-12 col-large-12">\n        <p>Add widget content here</p>\n        <span class="specific-style-text">\n          This is an example of specific widget only styling.\n          This ensures that the style is only applied to this widget and does not impact any other widget within Workspaces.\n        </span>\n      </div>\n    </div>\n  </div>\n\n</div>';
     var tpl_css = '/* Best Practice:\n   This is an example of the \'descendant selector\' css rule.\n   This ensures that the style is only applied to this widget and does not impact any other widget.\n*/\n.widget--<%= element %> .specific-style-text {\n  font-style: italic;\n}\n\n/* Another example which will impact all <span> elements within this widget only */\n.widget--<%= element %> span {\n  margin-top: 20px;\n}';
     var tpl_json = '{\n  "metadata": {\n    "name": "<%= name %>",\n    "description": "Please add widget description here",\n    "tags": "demo, widget, example, other",\n    "library": "<%= library %>",\n    "version": "1.0.0",\n    "date": "<%= date %>",\n    "id": "<%= id %>"\n  },\n  "configuration": {\n    "external": true,\n    "timeout": 5000,\n    "serie": true,\n    "name": "<%= id %>",\n    "element": "<<%= element %>></<%= element %>>",\n    "icon": "aoc-home",\n    "files": ["<%= element %>/<%= element %>.css", "<%= element %>/<%= element %>.js"]\n  }\n}';
-
+    var tpl_service_ts = 'class <%= className %> { \n http: ng.IHttpService; \n constructor($http: ng.IHttpService) { \n this.http = $http; \n } \n } \n angular.module(\'<%= id %>\').service(\'<%= serviceName %>\', <%= className %>);' 
+    
     var id = uuid();
     var name = grunt.option('name');
     var library_id = grunt.option('library') || 'Other';
-
+    var isBoolean = typeof grunt.option('service') === 'boolean';
+    let service;
+    isBoolean ? service = grunt.option('service') : (grunt.option('service') === 'true' ? service = true: service = false);
     if (!name) grunt.fail.fatal('Widget name is missing, please use: grunt create_widget --name="my-widget" --library="My library"');
     if (!name.includes('-')) grunt.fail.fatal('Dash "-" character missing from widget name, please use "param-case" for widget names, e.g, grunt create_widget --name="my-widget" --library="My library"');
 
@@ -253,6 +256,8 @@ module.exports = function(grunt) {
     var json_content = grunt.template.process(tpl_json, { data: { id: id, name: changeCase.titleCase(name), element: name, library: library_id, date: new Date().toISOString() } });
     var html_content = grunt.template.process(tpl_html, { data: { name: changeCase.titleCase(name), element: name } });
     var css_content = grunt.template.process(tpl_css, { data: { element: name } });
+    const className = changeCase.camelCase(name)[0].toUpperCase() + changeCase.camelCase(name).slice(1)
+    var ts_service_content = grunt.template.process(tpl_service_ts, { data: { id:id, className: className, serviceName: className } });
 
     grunt.file.mkdir(grunt.config('library_folder'));
     grunt.file.write(grunt.config('library_folder') + '/' + name + '/' + name + '.js', js_content);
@@ -260,6 +265,10 @@ module.exports = function(grunt) {
     grunt.file.write(grunt.config('library_folder') + '/' + name + '/' + name + '.html', html_content);
     grunt.file.write(grunt.config('library_folder') + '/' + name + '/' + name + '.css', css_content);
 
+    if (service) {
+      grunt.file.write(grunt.config('library_folder') + '/' + name + '/libs/' + name + '.service.ts', ts_service_content);
+    }
+    
     grunt.log.ok('The "' + name + '" widget has been created');
     grunt.log.subhead('To compile and bundle the widget: grunt --build="' + name + '"');
   });
